4fe5ed22a51105277d7da672d46250ebbe33d431
uellenberg <JonahUellenberg@hotmail.com>
2021-06-29 18:17:39 -0700

    Wrap users table in a scrollable box

--- client/pages/users/UsersPage.tsx
+++ client/pages/users/UsersPage.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import {UsersPageData} from "../../../shared/types/UsersPageTypes";
 import UsersTable from "./components/UsersTable";
+import {Box} from "@chakra-ui/react";
 
 const UsersPage = ({config, userInfo, data}) : JSX.Element => {
   const users = data as UsersPageData;
 
   return (
-    <UsersTable users={users.users} />
+    <Box
+      width="100%"
+      overflowX="auto"
+    >
+      <UsersTable users={users.users} />
+    </Box>
   );
 };
 
